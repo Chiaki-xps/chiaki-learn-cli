b2e7ab085c9b156512650df91b61eff999cc29e2
Chiaki-xps <1490295446@qq.com>
2023-03-09 14:47:48 +0800

    commander使用

--- index.js
+++ index.js
@@ -1,3 +1,14 @@
 #!/usr/bin/env node
-// 上面知道node程序执行下面代码
-console.log('why cli');
+
+const program = require('commander');
+
+// 输出版本号
+// require可以加载json文件，node文件
+// 函数第二个参数字符串，可以覆盖默认的参数解析
+// program.version(require('./package.json').version, '-v, --version');
+program.version(require('./package.json').version);
+
+//process全局变量，node进程相关信息
+// argv数组。命令行参数
+// program.parse解析参数后，执行对应的函数
+program.parse(process.argv);
